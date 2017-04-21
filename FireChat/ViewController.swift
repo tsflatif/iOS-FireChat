3af901716f3bf18250b1fcf51a0174d538b579a9
tsflatif <tsflatif@gmail.com>
2017-04-20 22:05:20 -0400

    Initial Commit

---
+++ FireChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireChat
+//
+//  Created by Tauseef Latif on 2017-04-20.
+//  Copyright © 2017 Tauseef Latif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
